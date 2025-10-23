ac51e9f112c48d8835132d6c3ddd4b6d751da860
Kyungbin Choi <iris122413@gmail.com>
2025-10-23 15:49:29 +0900

    check cold start cases

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 from scipy.sparse import csr_matrix
 
 def create_interaction_matrix(df: pd.DataFrame, user_col: str, item_col: str) -> csr_matrix:
@@ -8,3 +9,24 @@ def create_interaction_matrix(df: pd.DataFrame, user_col: str, item_col: str) ->
     # Create a user-item interaction matrix
     interaction_matrix = csr_matrix((df['rating'], (df[user_col], df[item_col])))
     return interaction_matrix
+
+def check_cold_start_users(train_df: pd.DataFrame, test_df: pd.DataFrame) -> list:
+    """
+    Check cold start users.
+    """
+    ucol = 'user_id'
+    # 고유 유저 추출 (벡터화)
+    train_users = train_df[ucol].astype(str).unique()
+    test_users  = test_df[ucol].astype(str).unique()
+    # 차집합 (정렬된 결과)
+    cold_users = np.setdiff1d(test_users, train_users, assume_unique=False)
+    return cold_users.tolist()
+
+def check_cold_start_items(train_df: pd.DataFrame, test_df: pd.DataFrame) -> list:
+    """
+    Check cold start items.
+    """
+    train_items = train_df["item_id"].astype(str).unique()
+    test_items  = test_df["item_id"].astype(str).unique()
+    cold_items = np.setdiff1d(test_items, train_items, assume_unique=False)
+    return cold_items.tolist()
